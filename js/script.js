a79c8610baedecb31f034ef2ace25124f649daf5
Marko Srdoč <markosrdoc@gmail.com>
2020-06-26 15:08:24 +0200

    Petak26.6.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,73 @@
-function getAllUrlParams(n){var e=n?n.split("?")[1]:window.location.search.slice(1),i={};if(e){e=e.split("#")[0];for(var s=e.split("&"),o=0;o<s.length;o++){var t=s[o].split("="),a=void 0,l=t[0].replace(/\[\d*\]/,function(n){return a=n.slice(1,-1),""}),r="undefined"==typeof t[1]||t[1];i[l]?("string"==typeof i[l]&&(i[l]=[i[l]]),"undefined"==typeof a?i[l].push(r):i[l][a]=r):i[l]=r}}return i}var $menubuton=$(".menubuton"),$menuwrapper=$(".mainmenuoverlay"),$menu=$(".mainmenu"),$pozicija=0;$menubuton.click(function(n){$("body").hasClass("menuout")?$(window).scrollTop($pozicija):($pozicija=$(window).scrollTop(),$menuwrapper.scrollTop(0)),$(this).toggleClass("menuclose"),$menuwrapper.toggleClass("visible"),$("body").toggleClass("menuout")});var previousScroll=0;$(window).scroll(function(){var n=$(this).scrollTop();n>previousScroll?$menubuton.removeClass("gore"):$menubuton.addClass("gore"),previousScroll=n}),$(".searchcall").click(function(n){$(".searchizlet").toggleClass("visiblesearch")}),$(window).resize(function(){}),$(".harmo").click(function(n){$(this).toggleClass("isOpen"),$(this).next(".nika").toggleClass("isDown")}),$(".harmo2").click(function(n){return n.preventDefault(),$(this).next(".nika").hasClass("isDown")?($(this).next(".nika").stop().animate({height:"hide"},500),$(this).next(".nika").removeClass("isDown")):($(".isDown").stop().animate({height:"hide"},250).removeClass("isDown"),$(this).next(".nika").stop().animate({height:"show"},500),$(this).next(".nika").addClass("isDown")),!1}),$(document).ready(function(){getAllUrlParams().print&&window.print(),$(".print").parents().addClass("printwrap")});
+var menubuton = document.getElementsByClassName("menubuton")[0];
+var menuwrapper = document.getElementsByClassName("mainmenuoverlay")[0];
+var menu = document.getElementsByClassName("mainmenu")[0];
+var searchcal = document.getElementsByClassName("searchcall")[0];
+var searchizlet = document.getElementsByClassName("searchizlet")[0];
+var pozicija = 0;
+
+menubuton.onclick = function () {
+  // console.log("click");
+  // document.body.classList.contains("menuout")
+  //   ? console.log("JE")
+  //   : console.log("NIJE");
+  menubuton.classList.toggle("menuclose");
+  menuwrapper.classList.toggle("visible");
+  document.body.classList.toggle("menuout");
+};
+
+// var previousScroll = 0;
+// window.onscroll = getScrollPosition;
+
+// function getScrollPosition() {
+//   var n = window.pageYOffset;
+//   n > previousScroll
+//     ? menubuton.classList.remove("gore")
+//     : menubuton.classList.add("gore");
+//   previousScroll = n;
+// }
+
+searchcal.onclick = function () {
+  searchizlet.classList.toggle("visiblesearch");
+};
+
+// harmo.onclick = function (e) {
+//   searchizlet.classList.toggle("visiblesearch");
+// };
+
+//   $('.harmo').click(function (n) {
+//     $(this).toggleClass('isOpen'), $(this).next('.nika').toggleClass('isDown')
+//   }),
+
+document.querySelectorAll(".harmo").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    //handle click
+    item.classList.toggle("isOpen");
+    item.nextElementSibling.classList.toggle("isDown");
+  });
+});
+
+document.querySelectorAll(".harmo2").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    //handle click
+    item.classList.toggle("isOpen");
+    item.nextElementSibling.classList.toggle("isDown");
+  });
+});
+
+//   $('.harmo2').click(function (n) {
+//     return (
+//       n.preventDefault(),
+//       $(this).next('.nika').hasClass('isDown')
+//         ? ($(this).next('.nika').stop().animate({ height: 'hide' }, 500),
+//           $(this).next('.nika').removeClass('isDown'))
+//         : ($('.isDown')
+//             .stop()
+//             .animate({ height: 'hide' }, 250)
+//             .removeClass('isDown'),
+//           $(this).next('.nika').stop().animate({ height: 'show' }, 500),
+//           $(this).next('.nika').addClass('isDown')),
+//       !1
+//     )
+//   }),
